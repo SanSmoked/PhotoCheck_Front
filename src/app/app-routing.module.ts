e3af57c22d95360f3f7fc865d386f32049ed4403
Gabriel Angel <gabrieljaime09@gmail.com>
2020-05-02 22:12:54 -0500

    se ponen validaciones al login y se crea el componente patient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PatientComponent } from './patient/patient.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'patient', component: PatientComponent }
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,51 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
-  });
+  loginForm: FormGroup;
 
-  constructor() {}
+  /* loginForm = new FormGroup({
+     email: new FormControl(''),
+     password: new FormControl('')
+   });
+ */
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required]
+    });
   }
 
-  login(data){}
+  public getError(controlName: string): string {
+    let error = '';
+    const control = this.loginForm.get(controlName);
+    if (control.touched && control.errors != null) {
+      switch (controlName) {
+        case 'email':
+          if (control.errors.required) {
+            //error = 'El email es requerido'
+            error = 'The email is required';
+          } else {
+            //error = 'el valor ingresado no corresponde a un email'
+            error = 'The input value is not valid for an email';
+          }
+          break;
+        case 'password':
+          error = 'The password is required';
+          break;
+
+        default:
+          break;
+      }
+    }
+    return error;
+  }
+
+  login(data) {
+    console.log(data);
+    this.router.navigate(['/patient']);
+  }
 
 }
