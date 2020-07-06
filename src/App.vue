7ccc9d5c3c3541e3fcd004100675eb1ef3d76513
Jose Aristizabal <j.aristizabal@astoundcommerce.com>
2020-07-06 18:09:18 -0500

    turn project to Vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to my crib :v"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  computed: {
+    isLoginPage () {
+      return this.$route.name ==='login'
+    }
+  },
+  
+}
+</script>
