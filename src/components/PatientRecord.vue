2c61ad9e027964bdae4925305d704372269c2f5f
Gabriel Angel <gabrieljaime09@gmail.com>
2020-08-05 10:41:00 -0500

    create folder added

--- src/components/PatientRecord.vue
+++ src/components/PatientRecord.vue
@@ -3,25 +3,55 @@
     <div class="list-group">
       <div class="card-header list-group-item logo">
         <img class="col-6" alt="Vue logo" src="@/assets/photoCheckLogo.svg" />
+        <v-btn @click="dialog = true" class="btn" rounded>Nuevo Folder</v-btn>
+        <v-dialog v-model="dialog" width="500">
+          <div class="modal-body">
+            <v-form ref="form" v-model="valid">
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                :rules="nameRules"
+                label="Nombre del Folder"
+                required
+              ></v-text-field>
+              <!--v-file-input v-model="files" multiple accept="image/*" label="File input"></v-file-input-->
+              <!--input @change="handleImage" multiple type="file" accept="image/*" /-->
+              <v-btn color="error" class="mr-4" @click="reset">Reset</v-btn>
+              <v-btn color="error" class="mr-4" @click="sendForm($route.params.cc)">Listo</v-btn>
+            </v-form>
+            <v-snackbar v-model="snackbar">
+              {{ text }}
+              <template v-slot:action="{ attrs }">
+                <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+              </template>
+            </v-snackbar>
+          </div>
+        </v-dialog>
       </div>
       <div class="list-group-item">
         <div class="row">
           <div class="personal-information">
-            <p class="col">Nombre: {{this.patientById($route.params.cc).nombre}}</p>
-            <p class="col">Edad: {{this.patientById($route.params.cc).edad}}</p>
-            <p class="col">{{this.patientById($route.params.cc).type}}: {{this.patientById($route.params.cc).documento}}</p>
+            <p class="col">
+              Nombre: {{this.patientById($route.params.cc).name}}
+              <br />
+              Edad: {{this.patientById($route.params.cc).age}}
+              <br />
+              {{this.patientById($route.params.cc).type}}: {{this.patientById($route.params.cc).document}}
+            </p>
           </div>
-          <!--img :src="this.patientById($route.params.cc).imageUrl" class="imgPatient" /-->
+          <img :src="this.patientById($route.params.cc).photo" class="imgPatient" />
         </div>
       </div>
-      <a class="list-group-item list-group-item-action" v-on:click="onclick()" v-for="record in this.getRecordsPerPatient($route.params.cc)" :key="record.id">
-        <div class="row" >
-          <img
-            src="@/assets/folder.png"
-            class="folder"
-          />
-          <p class="col-8">
-            <strong>{{record.desc}}</strong>
+      <a
+        class="list-group-item list-group-item-action"
+        v-for="record in this.getRecordsPerPatient($route.params.cc)"
+        :key="record.id"
+        v-on:click="onclick(record.id)"
+      >
+        <div class="row">
+          <img src="@/assets/folder.png" class="folder-img" />
+          <p class="col-8 folder">
+            <strong>{{record.title}}</strong>
           </p>
         </div>
       </a>
@@ -31,40 +61,77 @@
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import { db } from "../main";
 
 export default {
   name: "PatientRecord",
   computed: {
     ...mapState(["patients", "records"]),
-    ...mapGetters(["patientById", "getRecordsPerPatient"])
+    ...mapGetters(["patientById", "getRecordsPerPatient"]),
+  },
+  data() {
+    return {
+      dialog: false,
+      snackbar: false,
+      text: "Hello, I'm a snackbar",
+      valid: true,
+      name: "",
+      nameRules: [
+        (v) => !!v || "Name is required",
+        (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+      ],
+      files: [],
+      selectedImages: [],
+    };
   },
   methods: {
-    onclick() {
-      console.log(this.getRecordsPerPatient("1234"));
-
-      //this.$store.commit('getRecordsPerPatient');
-      //console.log(store.commit);
-      //this.$router.push("/patientRecord");
-    }
+    onclick(recordId) {
+      //this.getRecordsPerPatient(recordId)
+      console.log(recordId);
+    },
+    sendForm(patientId) {
+      console.log(patientId);
+      this.snackbar = true;
+      //const storageref = firebase.storage().ref(`/`)
+      //setTimeout(() => {}, 1000);
+      if (this.name) {
+        db.collection("records").add({
+          title: this.name,
+          patientId: patientId,
+          images: this.files,
+        });
+      } else {
+        console.log("todos los campos son necesarios");
+      }
+      this.dialog = false;
+      this.$forceUpdate()
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
   },
-  created() {
-    this.$store.commit("getRecords");
-  }
 };
 </script>
 
 <style scoped>
 .imgPatient {
-  height: 20%;
-  width: 20%;
+  height: 15%;
+  width: 15%;
   margin: auto;
 }
-
+.btn {
+  margin-left: 10%;
+}
 .logo {
   text-align: center;
 }
-
 .folder {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+.folder-img {
   height: 10%;
   width: 10%;
 }
--- src/store.js
+++ src/store.js
@@ -7,59 +7,50 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         records: [],
-        pokemons: [],
         patients: [],
         recordsPerPatient: []
     },
     mutations: {
-        fetchData(state) {
-            fetch("https://pokeapi.co/api/v2/pokemon")
-                .then(stream => stream.json())
-                .then(data => {
-                    data.results.forEach(pokemon => {
-                        let item = {};
-                        item.name = pokemon.name;
-                        item.id = (pokemon.url.split('/'))[6]
-                        item.imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + item.id + '.png'
-                        state.pokemons.push(item);
-                    })
-                });
-        },
-
         getPatients: state => {
             let patientsFb = [];
+            let date = new Date();
+            var year = date.getFullYear();
             db.collection('patients').onSnapshot((snapshot) => {
                 snapshot.forEach(doc => {
-                    patientsFb.push({ id: doc.id, type: doc.data().DocumentType, documento: doc.data().Document, nombre: doc.data().name, edad: doc.data().Edad })
+                    patientsFb.push({ id: doc.id, type: doc.data().documentType, document: doc.data().document, name: doc.data().name, age: (year - doc.data().birthdate.split('/')[0]), photo: doc.data().photo})
                 })
                 console.log(patientsFb);
                 state.patients = patientsFb;
             });
         },
         getRecords: state => {
-            let recordsFb = [];
+
             db.collection('records').onSnapshot((snapshot) => {
+                let recordsFb = [];
                 snapshot.forEach(doc => {
-                    recordsFb.push({ id: doc.id, patientId: doc.data().patientId, desc: doc.data().descripcion })
+                    recordsFb.push({ id: doc.id, patientId: doc.data().patientId, title: doc.data().title, images: doc.data().images })
                 })
-                console.log(recordsFb);
+                console.log('recordsFB', recordsFb);
                 state.records = recordsFb;
             })
-        }
+        },
     },
     actions: {
 
     },
     getters: {
         patientById: (state) => (id) => {
-            return state.patients.find(patient => patient.documento == id)
+            return state.patients.find(patient => patient.document == id)
         },
         getRecordsPerPatient: (state) => (document) => {
             return state.records.filter(record => record.patientId == document)
         },
         getRecordsPerPatientCount: (state, getters) => {
             return getters.getRecordsPerPatient.length
-          }
+        },
+        getFolderById: (state) => (folderId) => {
+            return state.records.find(record => record.id == folderId)
+        }
 
     }
 })
